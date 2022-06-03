74566edb3d4354af7c69839d00910900c6cb8af2
Peter Kimani <peterjajakim@gmail.com>
2022-06-03 23:12:03 +0300

    commit tree 7cf8621d39f923c52a51b23f0c31f6f05018ac4b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee ={
+    name: "Peter",
+    address: "Nairobi",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign({}, employee, {[key]: value});
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    return Object.assign(employee, {[key]: value});
+}
+function deleteFromEmployeeByKey(employee, key){
+    let newEmployee = {};
+    Object.assign(newEmployee, employee);
+    delete newEmployee [key];
+    return newEmployee;
+}
+
+function  destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee [key];
+    return employee;
+}
